ff20f6168970bd160a6c09a9c15fc5e198564e2c
erdemergul <m.erdemergul@gmail.com>
2021-10-21 19:15:43 +0300

    second commit

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -48,7 +48,7 @@ public class Jump : MonoBehaviour
             SceneManager.LoadScene("FlyScene");
         }
     }
-    PlayerPrefs.Save();
+    //PlayerPrefs.Save();
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "DeathArea")
